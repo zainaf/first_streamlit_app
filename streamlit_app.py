3847e7d21f934ef10695854c5f777008c93acd87
zainaf <zainafzal003@gmail.com>
2022-12-07 18:11:21 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,5 @@
 import streamlit 
-streamlit.title("This is Zain")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
